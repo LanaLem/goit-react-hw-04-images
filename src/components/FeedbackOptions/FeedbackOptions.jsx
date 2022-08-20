258b0a16ea6f59bb5a93519e4a546f3eb930646d
Svetlana Trotsenko <lanalemishko@gmail.com>
2022-08-20 13:59:38 +0300

    fix-keys-in-FeedbackOptions

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <Box as="ul" display="flex" justifyContent="center" alignItems="center">
-      {options.map((option, index) => (
-        <Box as="li" mr={3} key={index}>
+      {options.map(option => (
+        <Box as="li" mr={3} key={option}>
           <FeedbackButtom type="button" onClick={() => onLeaveFeedback(option)}>
             {option[0].toUpperCase() + option.slice(1)}
           </FeedbackButtom>
